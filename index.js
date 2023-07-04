24dea7ea98c1df3763e3df68a4e4eecb960b2135
Nelson Ramirez <xhyskoax@gmail.com>
2023-07-04 11:16:21 -0400

    avance 2

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import app from "./src/app.js";
+import { config } from "dotenv";
+config();
 
-const PORT = process.env.PORT || 3000;
-
-
+const PORT = process.env.PORT || 3001;
 
 const main = () => {
-
     app.listen(PORT, () =>
         console.log("Servidor escuchando en puerto: " + PORT)
     );
-}
+};
 
 main();
--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,7 +1,36 @@
+import * as path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export const findAllProductos = (req, res) => {
-    res.send("Ruta findAll Productos.")
-}
+    res.send("Ruta findAll Productos.");
+};
 
 export const addProductos = (req, res) => {
-    res.status(201).send("Ruta post productos.")
+    //console.log(req.body);
+    //console.log(req.files);
+    let foto = req.files.foto;
+    let formatosPermitidos = ["jpeg", "png", "webp", "gif", "svg"];
+    let extension = `${foto.mimetype.split("/")[1]}`;
+
+    if (!formatosPermitidos.includes(extension)) {
+        return res
+            .status(400)
+            .json({
+                code: 400,
+                message: `Formato no permitido ${extension}, formatos permitidos(${formatosPermitidos.join(" - ")})`,
+            });
+    }
+    let nombreFoto = `${Date.now()}-img.${extension}`;
+    let pathDestino = path.join(__dirname, "/../../public/uploads", nombreFoto);
+    //console.log(pathImagen);
+    //console.log(nombreFoto);
+    foto.mv(pathDestino, (error) => {
+        if (error) return res.status(500).json({ code: 500, message: "Error al subir la imagen." })
+        res.status(201).json({
+            code: 201,
+            message: "La imagen se cargó con éxito",
+            data: { img: nombreFoto },
+        });
+    });
 };
